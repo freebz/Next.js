9e1daeb9cec7d9aa2f2da6d57b035f9b71b45088
freebz <freebz@hananet.net>
2021-08-18 22:28:44 +0900

    2.1.3 동적 페이지 라우팅 하기

--- ch02/next-app/pages/index.jsx
+++ ch02/next-app/pages/index.jsx
@@ -1,17 +1,12 @@
 // '/' 경로를 가지는 페이지
 import Link from "next/link";
 
-//* 리액트 컴포넌트
-const Child = () => {
-  return <p>Move to '/tomato'</p>; 
-};
-
 const App = () => {
   return (
     <div>
-      <h2>Link to 'tomato' Page</h2>
-      <Link href="/tomato">
-        <Child />
+      <h2>Link to 'potato' Page</h2>
+      <Link href="/vegetable/potato">
+        <a>Move to '/vegetable/potato'</a>
       </Link>
     </div>
   );
