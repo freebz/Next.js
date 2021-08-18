893c2f23a8f371f4f940a300e544898c8bc77a66
freebz <freebz@hananet.net>
2021-08-18 22:22:04 +0900

    2.1.2 Link 컴포넌트

--- ch02/next-app/pages/index.jsx
+++ ch02/next-app/pages/index.jsx
@@ -1,12 +1,17 @@
 // '/' 경로를 가지는 페이지
 import Link from "next/link";
 
+//* 리액트 컴포넌트
+const Child = () => {
+  return <p>Move to '/tomato'</p>; 
+};
+
 const App = () => {
   return (
     <div>
       <h2>Link to 'tomato' Page</h2>
       <Link href="/tomato">
-        <p>Move to '/tomato'</p>
+        <Child />
       </Link>
     </div>
   );
