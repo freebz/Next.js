dc3586c10d43c7984d4e054ac88e4cd8c8ca58e7
freebz <freebz@hananet.net>
2021-08-18 22:58:23 +0900

    2.3 서버로부터 데이터 불러오기

--- ch02/next-app/pages/index.jsx
+++ ch02/next-app/pages/index.jsx
@@ -1,6 +1,22 @@
-const App = () => (
-  <div>
-    <img src="/cheese.jpg" alt="치즈" />
-  </div>
-);
-export default App;
+import fetch from "isomorphic-unfetch";
+
+const index = ({ user }) => {
+  const username = user && user.name;
+  return <div>{username}</div>;
+};
+
+export const getServerSideProps = async () => {
+  try {
+    const res = await fetch("https://api.github.com/users/jerrynim");
+    if (res.status === 200) {
+      const user = await res.json();
+      return { props: { user } };
+    }
+    return { props: {} };
+  } catch (e) {
+    console.log(e);
+    return { props: {} };
+  }
+};
+
+export default index;
