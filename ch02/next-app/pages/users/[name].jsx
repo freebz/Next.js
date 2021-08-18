af5de50b1c75034cb38e45317e90263adf2c0f36
freebz <freebz@hananet.net>
2021-08-19 07:09:00 +0900

    2.3.3 getInitialProps

--- ch02/next-app/pages/users/[name].jsx
+++ ch02/next-app/pages/users/[name].jsx
@@ -5,18 +5,18 @@ const name = ({ user }) => {
   return <div>{username}</div>;
 };
 
-export const getServerSideProps = async ({ query }) => {
+name.getInitialProps = async ({ query }) => {
   const { name } = query;
   try {
     const res = await fetch(`https://api.github.com/users/${name}`);
     if (res.status === 200) {
       const user = await res.json();
-      return { props: { user } };
+      return { user };
     }
-    return { props: {} };
+    return {};
   } catch (e) {
     console.log(e);
-    return { props: {} };
+    return {};
   }
 };
 
