8a9b7f24fbc35e8d53fb0e79ab7b84af5042592c
freebz <freebz@hananet.net>
2021-08-22 12:33:27 +0900

    2.5.1 _app파일

---
+++ ch02/next-app/pages/_app.jsx
@@ -0,0 +1,16 @@
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>
+        {`
+          body {
+            margin: 0;
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+export default MyApp;
