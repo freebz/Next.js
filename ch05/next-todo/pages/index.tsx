64b07f71839a90204255cf07315c9ba4ceebb90f
freebz <freebz@hananet.net>
2021-08-23 22:26:38 +0900

    TodoList 컴포넌트

--- ch05/next-todo/pages/index.tsx
+++ ch05/next-todo/pages/index.tsx
@@ -1,23 +1,9 @@
+import React from "react";
 import { NextPage } from "next";
-import styled from "styled-components";
+import TodoList from "../components/TodoList";
 
-const Container = styled.div`
-  padding: 20px;
-`;
-
-const index: NextPage = () => {
-  return (
-    <Container>
-      <h1>hello Styled-components</h1>
-      <h2>hello Styled-components</h2>
-      <p>hello Styled-components</p>
-      <ul>
-        <li>hello Styled-components</li>
-      </ul>
-      <a>hello Styled-components</a>
-      <span>hello Styled-components</span>
-    </Container>
-  );
+const app: NextPage = () => {
+  return <TodoList />;
 };
 
-export default index;
+export default app;
