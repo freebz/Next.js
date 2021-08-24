16d73945314be927a966255f8a3fda7c68672aac
freebz <freebz@hananet.net>
2021-08-25 00:01:56 +0900

    스타일드 컴포넌트를 사용하는 방법

--- ch05/next-todo/components/TodoList.tsx
+++ ch05/next-todo/components/TodoList.tsx
@@ -5,17 +5,15 @@ import { TodoType } from "../types/todo";
 
 const Container = styled.div`
   width: 100%;
-
-  .todo-list-header {
-    padding: 12px;
-    border-bottom: 1px solid ${palette.gray};
-
-    .todo-list-last-todo {
-      font-size: 14px;
-      span {
-        margin-left: 8px;
-      }
-    }
+`;
+const TodoHeader = styled.div`
+  padding: 12px;
+  border-bottom: 1px solid ${palette.gray};
+`;
+const TodoListLastNumber = styled.p`
+  font-size: 14px;
+  span {
+    margin-left: 8px;
   }
 `;
 
@@ -26,11 +24,11 @@ interface IProps {
 const TodoList: React.FC<IProps> = ({ todos }) => {
   return (
     <Container>
-      <div className="todo-list-header">
-        <p className="todo-list-last-todo">
+      <TodoHeader>
+        <TodoListLastNumber>
           남은 TODO<span>{todos.length}개</span>
-        </p>
-      </div>
+        </TodoListLastNumber>
+      </TodoHeader>
     </Container>
   );
 };
