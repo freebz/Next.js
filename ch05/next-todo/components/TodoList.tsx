de1967bf5c4bf0ede9cb3b0de357002bf28bbeb4
freebz <freebz@hananet.net>
2021-08-25 02:14:56 +0900

    5.2.1 색상별 투두리스트 개수 구하기

--- ch05/next-todo/components/TodoList.tsx
+++ ch05/next-todo/components/TodoList.tsx
@@ -5,15 +5,17 @@ import { TodoType } from "../types/todo";
 
 const Container = styled.div`
   width: 100%;
-`;
-const TodoHeader = styled.div`
-  padding: 12px;
-  border-bottom: 1px solid ${palette.gray};
-`;
-const TodoListLastNumber = styled.p`
-  font-size: 14px;
-  span {
-    margin-left: 8px;
+
+  .todo-list-header {
+    padding: 12px;
+    border-bottom: 1px solid ${palette.gray};
+
+    .todo-list-last-todo {
+      font-size: 14px;
+      span {
+        margin-left: 8px;
+      }
+    }
   }
 `;
 
@@ -22,13 +24,52 @@ interface IProps {
 }
 
 const TodoList: React.FC<IProps> = ({ todos }) => {
+  //* 색깔 객체 구하기 1
+  const getTodoColorNums = () => {
+    let red = 0;
+    let orange = 0;
+    let yellow = 0;
+    let green = 0;
+    let blue = 0;
+    let navy = 0;
+    todos.forEach((todo) => {
+      if (todo.color === "red") {
+        red += 1;
+      }
+      if (todo.color === "orange") {
+        orange += 1;
+      }
+      if (todo.color === "yellow") {
+        yellow += 1;
+      }
+      if (todo.color === "green") {
+        green += 1;
+      }
+      if (todo.color === "blue") {
+        blue += 1;
+      }
+      if (todo.color === "navy") {
+        navy += 1;
+      }
+    });
+    return {
+      red,
+      orange,
+      yellow,
+      green,
+      blue,
+      navy,
+    };
+  };
+  console.log(getTodoColorNums());
+
   return (
     <Container>
-      <TodoHeader>
-        <TodoListLastNumber>
+      <div className="todo-list-header">
+        <p className="todo-list-last-todo">
           남은 TODO<span>{todos.length}개</span>
-        </TodoListLastNumber>
-      </TodoHeader>
+        </p>
+      </div>
     </Container>
   );
 };
