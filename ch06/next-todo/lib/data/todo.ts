75e0456118584a08d27c5c40857117f0adaf2bda
freebz <freebz@hananet.net>
2021-08-30 00:28:34 +0900

    Todo Item의 checked를 변경한 후 todos.json에 저장

--- ch06/next-todo/lib/data/todo.ts
+++ ch06/next-todo/lib/data/todo.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import { TodoType } from "../../types/todo";
 
 //* 투두리스트 데이터 불러오기
@@ -19,4 +19,9 @@ const exist = ({ id }: { id: number }) => {
   return todo;
 };
 
-export default { getList, exist };
+//* 투두리스트 저장하기
+const write = async (todos: TodoType[]) => {
+  writeFileSync("data/todos.json", JSON.stringify(todos));
+};
+
+export default { getList, exist, write };
