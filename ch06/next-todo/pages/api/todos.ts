83fd2212ce163c26e7e6f92049f07de3b247be20
freebz <freebz@hananet.net>
2021-08-29 09:12:39 +0900

    동기식 방식으로 TODO 리스트 불러오기

--- ch06/next-todo/pages/api/todos.ts
+++ ch06/next-todo/pages/api/todos.ts
@@ -1,8 +1,24 @@
+import fs from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
+import { TodoType } from "../../types/todo";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
-    return res.send("hello Next");
+    try {
+      const todosBuffer = fs.readFileSync("data/todos.json");
+      const todosString = todosBuffer.toString();
+      if (!todosString) {
+        res.statusCode = 200;
+        res.send([]);
+      }
+      const todos: TodoType[] = JSON.parse(todosString);
+      res.statusCode = 200;
+      return res.send(todos);
+    } catch (e) {
+      console.log(e);
+      res.statusCode = 500;
+      res.send(e);
+    }
   }
 
   res.statusCode = 405;
