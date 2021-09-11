858025de82897da52ae009e5a021668b619813df
freebz <freebz@hananet.net>
2021-09-12 08:45:41 +0900

    10.4.1 유저 fs 함수 만들기

--- ch10/next-bnb/pages/api/auth/signup.ts
+++ ch10/next-bnb/pages/api/auth/signup.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import Data from "../../../lib/data";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
@@ -7,6 +8,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (!email || !firstname || !lastname || !password || !birthday) {
       res.statusCode = 400;
       return res.send("필수 데이터가 없습니다.");
+    }
+
+    const userExist = Data.user.exist({ email });
+    if (userExist) {
+      res.statusCode = 409;
+      res.send("이미 가입된 이메일입닏가.");
     }
 
     return res.end();
