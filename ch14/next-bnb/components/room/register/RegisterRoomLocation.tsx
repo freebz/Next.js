95721b4321f6301a37bb360a4b34a0532953e287
freebz <freebz@hananet.net>
2021-10-02 00:18:50 +0900

    주소 항목 리덕스 설정

--- ch14/next-bnb/components/room/register/RegisterRoomLocation.tsx
+++ ch14/next-bnb/components/room/register/RegisterRoomLocation.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
 import palette from "../../../styles/palette";
 import NavigationIcon from "../../../public/static/svg/register/navigation.svg";
 import Button from "../../common/Button";
 import Selector from "../../common/Selector";
 import { countryList } from "../../../lib/staticData";
 import Input from "../../common/Input";
+import { registerRoomActions } from "../../../store/registerRoom";
+import { useSelector } from "../../../store";
 
 const Container = styled.div`
   padding: 62px 30px 100px;
@@ -54,6 +57,51 @@ const Container = styled.div`
 `;
 
 const RegisterLocation: React.FC = () => {
+  const country = useSelector((state) => state.registerRoom.country);
+  const city = useSelector((state) => state.registerRoom.city);
+  const district = useSelector((state) => state.registerRoom.district);
+  const streetAddress = useSelector(
+    (state) => state.registerRoom.streetAddress
+  );
+  const detailAddress = useSelector(
+    (state) => state.registerRoom.detailAddress
+  );
+  const postcode = useSelector((state) => state.registerRoom.postcode);
+
+  const dispatch = useDispatch();
+
+  //* 나라 변경 시
+  const onChangeCountry = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(registerRoomActions.setCountry(event.target.value));
+  };
+
+  //* 시/도 변경 시
+  const onChangeCity = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(registerRoomActions.setCity(event.target.value));
+  };
+
+  //* 시/군/구 변경 시
+  const onChangeDistrict = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(registerRoomActions.setDistrict(event.target.value));
+  };
+
+  //* 도로명주소 변경 시
+  const onChangeStreetAddress = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(registerRoomActions.setStreetAddress(event.target.value));
+  };
+
+  //* 동호수 변경 시
+  const onChangeDetailAddress = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    dispatch(registerRoomActions.setDetailAddress(event.target.value));
+  };
+
+  //* 우편번호 변경 시
+  const onChangePostcode = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(registerRoomActions.setPostcode(e.target.value));
+  };
+
   return (
     <Container>
       <h2>숙소의 위치를 알려주세요.</h2>
@@ -73,20 +121,31 @@ const RegisterLocation: React.FC = () => {
           useValidation={false}
           defaultValue="국가/지역 선택"
           disabledOptions={["국가/지역 선택"]}
+          value={country}
+          onChange={onChangeCountry}
         />
       </div>
       <div className="register-room-location-city-district">
-        <Input label="시/도" />
-        <Input label="시/군/구" />
+        <Input label="시/도" value={city} onChange={onChangeCity} />
+        <Input label="시/군/구" value={district} onChange={onChangeDistrict} />
       </div>
       <div className="register-room-location-street-address">
-        <Input label="도로명주소" />
+        <Input
+          label="도로명주소"
+          value={streetAddress}
+          onChange={onChangeStreetAddress}
+        />
       </div>
       <div className="register-room-location-detail-address">
-        <Input label="동호수(선택 사항)" useValidation={false} />
+        <Input
+          label="동호수(선택 사항)"
+          value={detailAddress}
+          onChange={onChangeDetailAddress}
+          useValidation={false}
+        />
       </div>
       <div className="register-room-location-postcode">
-        <Input label="우편번호" />
+        <Input label="우편번호" value={postcode} onChange={onChangePostcode} />
       </div>
     </Container>
   );
