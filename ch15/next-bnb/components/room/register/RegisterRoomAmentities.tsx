d7b67b06a8ad280d686e8154086f7f5b839d36e5
freebz <freebz@hananet.net>
2021-10-04 21:01:04 +0900

    15.3 숙소 편의 시설에 체크박스 공통 컴포넌트 적용하기

--- ch15/next-bnb/components/room/register/RegisterRoomAmentities.tsx
+++ ch15/next-bnb/components/room/register/RegisterRoomAmentities.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
+
+import RegisterRoomFooter from "./RegisterRoomFooter";
 import palette from "../../../styles/palette";
+import { useSelector } from "../../../store";
+import { registerRoomActions } from "../../../store/registerRoom";
+import { amentityList } from "../../../lib/staticData";
+import CheckboxGroup from "../../common/CheckboxGroup";
 
 const Container = styled.div`
   padding: 62px 30px 100px;
@@ -22,6 +29,14 @@ const Container = styled.div`
 `;
 
 const RegisterRoomAmentities: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const amentities = useSelector((state) => state.registerRoom.amentities);
+
+  const onChangeAmentities = (selected: string[]) => {
+    dispatch(registerRoomActions.setAmentities(selected));
+  };
+
   return (
     <Container>
       <h2>어떤 편의 시설을 제공하시나요?</h2>
@@ -30,6 +45,17 @@ const RegisterRoomAmentities: React.FC = () => {
         일반적으로 게스트가 기대하는 편의 시설 목록입니다. 숙소를 등록한 후
         언제든 편의 시설을 추가할 수 있어요.
       </p>
+      <div className="register-room-amentities-checkbox-group-wrapper">
+        <CheckboxGroup
+          value={amentities}
+          onChange={onChangeAmentities}
+          options={amentityList}
+        />
+      </div>
+      <RegisterRoomFooter
+        prevHref="/room/register/location"
+        nextHref="/room/register/conveniences"
+      />
     </Container>
   );
 };
