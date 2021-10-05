20015e94994149c39cd79735d8c9ddb01f44fb4c
freebz <freebz@hananet.net>
2021-10-06 00:02:06 +0900

    Location 값을 결과값으로 반환

--- ch17/next-bnb/pages/api/files/upload.ts
+++ ch17/next-bnb/pages/api/files/upload.ts
@@ -13,25 +13,29 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     try {
       const form = new formidable.IncomingForm();
-      form.parse(req, async (err, fields, files) => {
-        const s3 = new aws.S3({
-          accessKeyId: process.env.ACCESSKEY_ID,
-          secretAccessKey: process.env.SECRET_ACCESSKEY_ID,
+      const url = await new Promise((resolve, reject) => {
+        form.parse(req, async (err, fields, files) => {
+          const s3 = new aws.S3({
+            accessKeyId: process.env.ACCESSKEY_ID,
+            secretAccessKey: process.env.SECRET_ACCESSKEY_ID,
+          });
+
+          const stream = createReadStream(files.file.path);
+
+          await s3
+            .upload({
+              Bucket: process.env.S3_BUCKET_NAME!,
+              Key: files.file.name,
+              ACL: "public-read",
+              Body: stream,
+            })
+            .promise()
+            .then((res) => resolve(res.Location))
+            .catch((e) => reject(e));
         });
-
-        const stream = createReadStream(files.file.path);
-
-        await s3
-          .upload({
-            Bucket: process.env.S3_BUCKET_NAME!,
-            Key: files.file.name,
-            ACL: "public-read",
-            Body: stream,
-          })
-          .promise()
-          .then((res) => console.log(res))
-          .catch((e) => console.log(e));
       });
+      res.statusCode = 201;
+      res.send(url);
     } catch (e) {
       console.log(e);
       res.end();
