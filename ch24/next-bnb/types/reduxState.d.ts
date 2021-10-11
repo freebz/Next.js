5d33ae18d9879828a69ebfc6df58afafece3aa73
freebz <freebz@hananet.net>
2021-10-12 00:58:03 +0900

    24장 숙소 리스트 페이지

---
+++ ch24/next-bnb/types/reduxState.d.ts
@@ -0,0 +1,23 @@
+import { UserType } from "./user";
+
+//* 유저 redux state
+export type UserState = UserType & {
+  isLogged: boolean;
+};
+
+//*공통 redux state
+export type CommonState = {
+  validateMode: boolean;
+};
+
+//* 숙소 검색 redux state
+export type SearchRoomState = {
+  location: string;
+  latitude: number;
+  longitude: number;
+  checkInDate: string | null;
+  checkOutDate: string | null;
+  adultCount: number;
+  childrenCount: number;
+  infantsCount: number;
+};
